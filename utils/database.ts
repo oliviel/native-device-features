469e9f2a61a5ddd4391dcfec2b7d664defc77dfc
oliviel <olivielultimate@gmail.com>
2023-11-07 18:17:50 -0400

    Add database

---
+++ utils/database.ts
@@ -0,0 +1,31 @@
+import * as SQLite from "expo-sqlite";
+
+const database = SQLite.openDatabase("places.db");
+
+export function init() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
+      id: INTEGER PRIMARY KEY NOT NULL,
+      title TEXT NOT NULL,
+      imageUrl TEXT NOT NULL,
+      address TEXT NOT NULL,
+      lat REAL NOT NULL,
+      lng REAL NOT NULL
+    )`,
+        [],
+        () => {
+          resolve("success");
+        },
+        (_, error) => {
+          console.log("ERROR ===", error);
+          reject(error);
+          return false;
+        }
+      );
+    });
+  });
+
+  return promise;
+}
